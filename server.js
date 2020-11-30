d9ae29f1f5837adebeed79de385559d2cf526ad8
Glitch (hello-express) <none>
2020-11-30 14:19:42 +0000

    🏋🚃 Checkpoint ./server.js:18907310/706

--- server.js
+++ server.js
@@ -31,9 +31,35 @@ client.on('message', message => {
 
       .setDescription("test") 
 
-     .setColor("RANDOM") 
+     .setColor("RED")
+      message.channel.send("هل تريد قائمة الهيلب") 
+.then(msg => {
+
+        msg.react('✅')
+  
+
+let k = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
+
+let e = msg.createReactionCollector(k, { time: 20000 });
+
+       
+
+e.on("collect", r => {
+
 
 message.author.send(w) 
    
-    message.channel.send("") 
-}}) 
+    
+    message.delete()
+  msg.edit("تم الإرسال ") 
+  msg.reactions.removeAll()
+  .catch(console.error)
+     })
+
+    })
+
+}
+
+});
+
+
